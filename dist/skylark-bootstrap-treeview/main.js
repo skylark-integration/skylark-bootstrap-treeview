97005af7abc99db7c607d8fda03e9373a21f40ec
hudaokeji <github@hudaokeji.com>
2019-10-05 18:33:16 +0900

    init commit

---
+++ dist/skylark-bootstrap-treeview/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-bootstrap-treeview - A version of bootstrap treeview that ported to running on skylarkjs ui.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-bootstrap-treeview/
+ * @license MIT
+ */
+define(["./TreeView"],function(e){return e});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ src/main.js
@@ -0,0 +1,5 @@
+define([
+	"./TreeView"
+],function(TreeView){
+	return TreeView;
+});
